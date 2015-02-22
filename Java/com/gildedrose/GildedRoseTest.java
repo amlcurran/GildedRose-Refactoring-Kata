3fc2975280c08badeaaf91ca0ed6850309665f51
Alex Curran <aml.curran@gmail.com>
2015-02-22 17:29:34 +0000

    Initial test on the first requirement

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,25 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void oncePastTheSellByDate_QualityDegradesTwiceAsFast() {
+        int startingQuality = 10;
+        Item normalItem = new Item("Cheese", 1, startingQuality);
+
+        GildedRose gildedRose = new GildedRose(new Item[]{normalItem});
+        gildedRose.updateQuality();
+
+        assertThat(normalItem.quality, is(startingQuality - 1));
+
+        gildedRose.updateQuality();
+
+        assertThat(normalItem.quality, is(startingQuality - 3));
     }
 
 }
