76e4ababb7ac2dddc94a068b3abd6f903e1ae487
Alex Curran <aml.curran@gmail.com>
2015-02-22 22:05:23 +0000

    Beginning to push responsibility into items

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -13,51 +13,22 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(BRIE) && !item.name.equals(BACKSTAGE_PASS)) {
-                decrementValue(item);
-            } else {
-                incrementValue(item);
-                if (item.name.equals(BACKSTAGE_PASS)) {
-                    if (item.sellIn < 11) {
-                        incrementValue(item);
-                    }
-
-                    if (item.sellIn < 6) {
-                        incrementValue(item);
-                    }
-                }
-            }
-
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (isPassSellByDate(item)) {
-                if (item.name.equals(BRIE)) {
-                    incrementValue(item);
-                } else {
-                    if (item.name.equals(BACKSTAGE_PASS)) {
-                        item.quality = 0;
-                    } else {
-                        decrementValue(item);
-                    }
-                }
-            }
+            item.update();
         }
     }
 
-    private static boolean isPassSellByDate(Item item) {
+    public static boolean isPassSellByDate(Item item) {
         return item.sellIn < 0;
     }
 
-    private void decrementValue(Item item) {
+    public static void decrementValue(Item item) {
         if (item.quality > 0 && !item.name.equals(SULFURAS)) {
             int decrement = item.name.equals(CONJURED) ? 2 : 1;
             item.quality = item.quality - decrement;
         }
     }
 
-    private void incrementValue(Item item) {
+    public static void incrementValue(Item item) {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
--- Java/com/gildedrose/Item.java
+++ Java/com/gildedrose/Item.java
@@ -18,4 +18,37 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    public void update() {
+        if (!name.equals(GildedRose.BRIE) && !name.equals(GildedRose.BACKSTAGE_PASS)) {
+            GildedRose.decrementValue(this);
+        } else {
+            GildedRose.incrementValue(this);
+            if (name.equals(GildedRose.BACKSTAGE_PASS)) {
+                if (sellIn < 11) {
+                    GildedRose.incrementValue(this);
+                }
+
+                if (sellIn < 6) {
+                    GildedRose.incrementValue(this);
+                }
+            }
+        }
+
+        if (!name.equals(GildedRose.SULFURAS)) {
+            sellIn = sellIn - 1;
+        }
+
+        if (GildedRose.isPassSellByDate(this)) {
+            if (name.equals(GildedRose.BRIE)) {
+                GildedRose.incrementValue(this);
+            } else {
+                if (name.equals(GildedRose.BACKSTAGE_PASS)) {
+                    quality = 0;
+                } else {
+                    GildedRose.decrementValue(this);
+                }
+            }
+        }
+    }
 }
